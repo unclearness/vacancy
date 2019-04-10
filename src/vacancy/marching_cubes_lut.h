235cf5d8296dc4f27d81b34aacfebe7657699716
unlclearness <example@example.com>
2019-04-11 08:29:46 +0900

    add const

--- src/vacancy/marching_cubes_lut.h
+++ src/vacancy/marching_cubes_lut.h
@@ -12,7 +12,7 @@
 
 namespace vacancy {
 namespace marching_cubes_lut {
-extern std::array<int, 256> kEdgeTable;
-extern std::array<std::array<int, 16>, 256> kTriTable;
+extern const std::array<int, 256> kEdgeTable;
+extern const std::array<std::array<int, 16>, 256> kTriTable;
 }  // namespace marching_cubes_lut
 }  // namespace vacancy
