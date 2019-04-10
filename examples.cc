385ccb580d75b286f8fb0683679d39f3a8cbbf0f
unlclearness <example@example.com>
2019-04-10 21:38:36 +0900

    const ref

--- examples.cc
+++ examples.cc
@@ -8,7 +8,7 @@
 
 #include "vacancy/voxel_carver.h"
 
-std::vector<std::string> split(std::string& input, char delimiter) {
+std::vector<std::string> split(const std::string& input, char delimiter) {
   std::istringstream stream(input);
   std::string field;
   std::vector<std::string> result;
